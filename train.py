85f3e867727eb50c36a54d5510a56792789019a6
Nguyen Van Tam <tamnvhustcc@gmail.com>
2022-08-26 10:34:17 +0700

    Using auto mode

--- train.py
+++ train.py
@@ -1,12 +1,12 @@
-import os
-import argparse
-
-import torch
-import pytorch_lightning as pl
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-
-from utils.data import GOSDataModule
 from model.isnet import ISNet
+from utils.data import GOSDataModule
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+import pytorch_lightning as pl
+
+import argparse
+import warnings
+
+warnings.simplefilter('ignore')
 
 
 def train():
@@ -20,17 +20,12 @@ def train():
 
     # Training
     print('Started training')
-    accelerator = 'cpu'
-    devices = 1
-    if torch.cuda.is_available():
-        accelerator = 'gpu'
-        devices = min(torch.cuda.device_count(), len(args.gpus.split(',')))
     trainer = pl.Trainer(
-        devices=devices,
-        accelerator=accelerator,
+        devices='auto',
+        accelerator='auto',
         max_epochs=args.epochs,
         callbacks=[EarlyStopping(
-            monitor='val_f1_score', mode='max', patience=3, verbose=True)]
+            monitor='val_f1_score', mode='max', patience=5, verbose=True)]
     )
     trainer.fit(
         model=model,
@@ -42,8 +37,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     # Training
-    parser.add_argument('--gpus', type=str, default='0',
-                        help='A list of GPUs e.g 0,1,2,3. Use None for CPU only')
     parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate')
     parser.add_argument('--batch_size', type=int, default=4,
                         help='Batch size. If training on n GPUs, it should be set'
