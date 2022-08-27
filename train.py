b38b85b4c4c99c0995d582fdaf88ed075c88f92a
Nguyen Van Tam <tamnvhustcc@gmail.com>
2022-08-27 07:04:08 +0700

    Changed arguments

--- train.py
+++ train.py
@@ -1,12 +1,26 @@
-from model.isnet import ISNet
-from utils.data import GOSDataModule
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-import pytorch_lightning as pl
-
+from pathlib import Path
 import argparse
 import warnings
 
+from model.isnet import ISNet
+from utils.data import GOSDataModule
+import pytorch_lightning as pl
+
 warnings.simplefilter('ignore')
+
+
+def load_checkpoint(checkpoint_dir):
+    logs_dir = Path(checkpoint_dir) / 'lightning_logs'
+    if not logs_dir.exists():
+        return
+
+    version_dirs = sorted(logs_dir.glob('version_*'))
+    if len(version_dirs):
+        latest_version_dir = version_dirs[-1]
+        actual_checkpoint_dir = latest_version_dir / 'checkpoints'
+        checkpoint_paths = sorted(actual_checkpoint_dir.glob('*.ckpt'))
+        if len(checkpoint_paths):
+            return str(checkpoint_paths[-1])
 
 
 def train():
@@ -20,16 +34,17 @@ def train():
 
     # Training
     print('Started training')
+    # checkpoint_path = load_checkpoint(args.checkpoint_dir)
     trainer = pl.Trainer(
         devices='auto',
         accelerator='auto',
         max_epochs=args.epochs,
-        callbacks=[EarlyStopping(
-            monitor='val_f1_score', mode='max', patience=5, verbose=True)]
+        default_root_dir=args.root_dir,
     )
     trainer.fit(
         model=model,
         datamodule=data_module,
+        ckpt_path=args.ckpt_path,
     )
 
 
@@ -47,6 +62,10 @@ if __name__ == '__main__':
                         help='Number of data loading workers.')
     parser.add_argument('--outdir', type=str, default='output',
                         help='Training output directory.')
+    parser.add_argument('--root_dir', type=str, default='.',
+                        help='Logs directory.')
+    parser.add_argument('--ckpt_path', type=str, default=None,
+                        help='Checkpoints directory')
 
     # Data
     parser.add_argument('--data_dir', type=str, help='Path to root data dir')
